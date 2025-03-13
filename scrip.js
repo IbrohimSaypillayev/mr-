bf3066774ea7567f6c9007356cb84e0741062286
IbrohimSaypillayev <sarahconoruzb@gmail.com>
2025-03-13 09:04:22 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,13 @@
+let ismingiz = prompt("ismingiz kiriting")
+
+let familyez = prompt("familyezni kiriting")
+
+let yoshingiz = prompt("yoshingizni kiriting")
+
+let yashaydigan =prompt("yashaydigan joyingiz")
+
+let yoqtirgan= prompt("yoqtirgan faningiz yozing")
+
+alert("malumot kiritganingiz uchun rahmat")
+
+alert(`meni ismim ${ ismingiz},meni${ yoshingiz } yosh`)
